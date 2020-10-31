5a888e44f731d3ac551002b680def9249f3a3bd0
lucasliet <lucasouliveira@gmail.com>
2020-10-31 02:49:17 -0300

    Updates

--- ormconfig.js
+++ ormconfig.js
@@ -17,10 +17,10 @@ if(process.env.NODE_ENV == 'development'){
       "type": "postgres",
       "url": process.env.DATABASE_URL,
       "migrations": [
-        "./src/database/migrations/*.ts"
+        "./src/database/migrations/*.js"
       ],
       "entities": [
-        "./src/models/*.ts"
+        "./src/models/*.js"
       ],
       "cli": {
         "migrationsDir": "./src/database/migrations"
