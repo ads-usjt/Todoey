0bcd01b0f1f40ea578be9431f4da1df7ccb85096
lucasliet <lucasouliveira@gmail.com>
2020-11-16 23:27:12 -0300

    move login function to user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
 
 export default {
@@ -59,6 +61,27 @@ export default {
     await userRepository.save( updatedUser );
 
     return response.json({ id, name, email });
+  },
+
+  async login (request: Request, response: Response){
+
+    const userRepository = getRepository(User);
+
+    const { email, password } = request.body;
+
+    const user = await userRepository.findOne({ where: { email, password } });
+
+    if(user){
+
+      const { id } = user;
+      const token = jwt.sign({ id }, process.env.SECRET as string, {
+        expiresIn: 3000
+      });
+
+      return response.json({ auth: true, token: token });
+    }
+    return response.status(500).json({message: 'Invalid Login!'});
+
   }
 
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import LoginController from './controllers/LoginController';
 import ReminderController from './controllers/ReminderController';
 import UserController from './controllers/UserController';
 
@@ -19,7 +18,7 @@ routes.put('/reminders/:id', ReminderController.update);
 
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
-routes.post('/login', LoginController.login);
+routes.post('/login', UserController.login);
 routes.post('/users', UserController.create);
 routes.put('/users/:id', UserController.update);
 
