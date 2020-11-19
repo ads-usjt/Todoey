49815f2865207286c3b5f124babfe5b27459b3f0
lucasliet <lucasouliveira@gmail.com>
2020-11-19 13:31:45 -0300

    Updates

--- src/models/Reminder.js
+++ src/models/Reminder.js
@@ -21,7 +21,7 @@ __decorate([
     __metadata("design:type", Number)
 ], Reminder.prototype, "id", void 0);
 __decorate([
-    typeorm_1.Column(),
+    typeorm_1.Column({ length: 25 }),
     __metadata("design:type", String)
 ], Reminder.prototype, "title", void 0);
 __decorate([
--- src/models/User.js
+++ src/models/User.js
@@ -21,11 +21,11 @@ __decorate([
     __metadata("design:type", Number)
 ], User.prototype, "id", void 0);
 __decorate([
-    typeorm_1.Column(),
+    typeorm_1.Column({ length: 50 }),
     __metadata("design:type", String)
 ], User.prototype, "name", void 0);
 __decorate([
-    typeorm_1.Column(),
+    typeorm_1.Column({ unique: true }),
     __metadata("design:type", String)
 ], User.prototype, "email", void 0);
 __decorate([
